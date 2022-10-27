47f99acdc2d8215d12fbc1e5faceb8b8ffbe624f
Andres Plana <dudeandu@hotmail.com>
2022-10-27 00:25:23 -0400

    changed the nav styles to allow for a menu to drop down and then created a click function that will opne it

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("active")
+const burgerButton = document.getElementById('hamburger');
+const navList = document.getElementById('navList');
+
+burgerButton.addEventListener('click', function(){
+  navList.style.height = '130px';
+  console.log(this)
+}); 
