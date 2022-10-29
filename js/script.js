21e53b6b8a8a0808867c91ffc8df211c8ccd7ee4
Andres Plana <dudeandu@hotmail.com>
2022-10-28 22:38:56 -0400

    the nav burger is interactive now

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const burgerButton = document.getElementById('hamburger');
 const navList = document.getElementById('navList');
+const navButtonIcon = document.querySelector('#hamburger i')
+console.log(navButtonIcon)
 
 burgerButton.addEventListener('click', function(){
-  navList.style.height = '130px';
+  // navList.style.height = '130px';
+  navList.classList.toggle('expandedNav')
+  navButtonIcon.classList.toggle('fa-xmark')
+  navButtonIcon.classList.toggle('fa-bars')
   console.log(this)
 }); 
